245afa562f3e888e8a6ad28d417f22811d442c73
angelique-df <faye.angelique.d@gmail.com>
2022-08-12 18:03:04 +0000

    Fix `ContactForm` render method

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -9,7 +9,40 @@ export const ContactForm = ({
 	setEmail,
 	handleSubmit,
 }) => {
-  return (
-    ContactForm
-  );
+	return (
+		<form onSubmit={handleSubmit}>
+			<label>
+				name
+				<input
+					type="text"
+					name="name"
+					value={name}
+					onChange={(e) => setName(e.target.value)}
+				/>
+			</label>
+
+			<label>
+				phone
+				<input
+					type="tel"
+					name="phone"
+					value={phone}
+					onChange={(e) => setPhone(e.target.value)}
+					pattern="[1-9][0-9]{2}-[1-9][0-9]{2}-[0-9]{4}"
+				/>
+			</label>
+
+			<label>
+				email
+				<input
+					type="email"
+					name="email"
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+				/>
+			</label>
+
+			<input type="submit" />
+		</form>
+	);
 };
