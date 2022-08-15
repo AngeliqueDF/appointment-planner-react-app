38014d870aeb4ad9052afec8711de612754e57aa
angelique-df <faye.angelique.d@gmail.com>
2022-08-15 17:49:37 +0000

    Render `input` elements

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -1,25 +1,46 @@
 import React from "react";
 
 export const AppointmentForm = ({
-  contacts,
-  title,
-  setTitle,
-  contact,
-  setContact,
-  date,
-  setDate,
-  time,
-  setTime,
-  handleSubmit
+	contacts,
+	title,
+	setTitle,
+	contact,
+	setContact,
+	date,
+	setDate,
+	time,
+	setTime,
+	handleSubmit,
 }) => {
-  const getTodayString = () => {
-    const [month, day, year] = new Date()
-      .toLocaleDateString("en-US")
-      .split("/");
-    return `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
-  };
+	const getTodayString = () => {
+		const [month, day, year] = new Date()
+			.toLocaleDateString("en-US")
+			.split("/");
+		return `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
+	};
 
-  return (
-    AppointmentForm
-  );
+	return (
+		<form onSubmit={handleSubmit}>
+			<input
+				type="text"
+				name="title"
+				value={title}
+				onChange={(e) => setTitle(e.target.value)}
+			/>
+			<input
+				min={getTodayString()}
+				type="date"
+				name="date"
+				value={date}
+				onChange={(e) => setDate(e.target.value)}
+			/>
+			<input
+				type="time"
+				name="time"
+				value={time}
+				onChange={(e) => setTime(e.target.value)}
+			/>
+			<input type="submit" />
+		</form>
+	);
 };
