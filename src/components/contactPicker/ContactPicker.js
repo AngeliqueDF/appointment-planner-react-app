cf1d004831b2b28a7532fa5224115a2095867cc9
angelique-df <faye.angelique.d@gmail.com>
2022-08-15 17:51:55 +0000

    Update ContactPicker.js

--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -1,7 +1,15 @@
 import React from "react";
 
-export const ContactPicker = () => {
-  return (
-    ContactPicker
-  );
+export const ContactPicker = ({ contact, contacts, onChange }) => {
+	const defaultOption = "Choose a contact";
+	return (
+		<select value={contact || defaultOption} onChange={onChange}>
+			<option value={defaultOption}>{defaultOption}</option>
+			{contacts.map((storedContact) => (
+				<option key={storedContact.id} value={storedContact.name}>
+					{storedContact.name}
+				</option>
+			))}
+		</select>
+	);
 };
