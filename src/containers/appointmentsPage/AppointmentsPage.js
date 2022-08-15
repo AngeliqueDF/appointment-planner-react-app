ce97dee2e07ce87e81103de5c9f7b6b9c26cd3c0
angelique-df <faye.angelique.d@gmail.com>
2022-08-15 17:43:21 +0000

    Add state variables

--- src/containers/appointmentsPage/AppointmentsPage.js
+++ src/containers/appointmentsPage/AppointmentsPage.js
@@ -12,10 +12,17 @@ export const AppointmentsPage = ({
   Define state variables for 
   appointment info
   */
+	const [title, setTitle] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     /*
+	const [contact, setContact] = useState("");
+
+	const [date, setDate] = useState("");
+
+	const [time, setTime] = useState("");
+
     Add contact info and clear data  
     */
    
